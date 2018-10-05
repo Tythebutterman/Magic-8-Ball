e295df28485006d3eb3d8155977e95f99e82c22d
Tyrese <tyrese.hughey.2021@student.lhschools.org>
2018-10-05 08:02:09 -0400

    Made it even better

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -7,52 +7,54 @@ public class Magic8Ball {
         Scanner keyboard;
         int testing;
         String question;
+        int pickednumber;
 
 
 
         keyboard=new Scanner(System.in);
 
+        pickednumber=(int)(Math.random()*100);
+
         System.out.println("Ask the magic 8 ball a question  ");
         question=keyboard.nextLine();
-        System.out.println("write a number 0-100");
-        testing=keyboard.nextInt();
-        if (testing<11) {
+
+        if (pickednumber<11) {
 
             System.out.println("YEZZIR");
         }
-        else if (testing<21) {
+        else if (pickednumber<21) {
 
             System.out.println("No way");
         }
-        else if (testing<31) {
+        else if (pickednumber<31) {
 
             System.out.println("Thats all you");
         }
-        else if (testing<41) {
+        else if (pickednumber<41) {
 
             System.out.println("Thats not possible");
         }
-        else if (testing<51) {
+        else if (pickednumber<51) {
 
             System.out.println("Ask another question");
         }
-        else if (testing<61) {
+        else if (pickednumber<61) {
 
             System.out.println("No doubt about it");
         }
-        else if (testing<71) {
+        else if (pickednumber<71) {
 
             System.out.println("As I see it, yes");
         }
-        else if (testing<81) {
+        else if (pickednumber<81) {
 
             System.out.println("It is certain");
         }
-        else if (testing<91) {
+        else if (pickednumber<91) {
 
             System.out.println("Not a chance");
         }
-        else if (testing<101) {
+        else if (pickednumber<101) {
 
             System.out.println("NOZZIR");
         }
